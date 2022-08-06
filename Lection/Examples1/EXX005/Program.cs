dcccde7241ee763e199ccdd02ef10abeab030284
Avanes <avanesovkl@mail.ru>
2022-08-07 00:31:16 +1100

    Init Repo Leraning c#

---
+++ Lection/Examples1/EXX005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Input name:");
+string username = Console.ReadLine();
+
+if (username == "Mary")
+{
+    Console.WriteLine("Hello Mary");
+}
+else 
+{
+    Console.Write("Hello: ");
+    Console.WriteLine(username);  
+}
---
+++ Seminar/Seminar1/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear(); //For all exx
+
+//Exx01
+// Console.Clear();
+// int number = Convert.ToInt32(Console.ReadLine());
+// int square = number * number;
+// //int result = Convert.ToInt32(Math.Pow(number, 2));
+// System.Console.WriteLine("Square : " + square);
+
+
+// //Exx02
+// Console.Write("Input number #1: ");
+// int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write(value: "Input number #2: ");
+// int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+// if (firstnumber == secondnumber * secondnumber)
+// {
+//     System.Console.WriteLine("Firstnumber = power: " + secondnumber);
+// }
+// else System.Console.WriteLine("Firstnumber =  not power: " + secondnumber);
+
+//Exx03
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = number * (-1);
+while (i <= number)
+{
+ System.Console.Write(i + " ");   
+i++;
+}
