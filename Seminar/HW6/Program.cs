a107f2198a811932da19115f657d479a2464d859
Avanes <avanesovkl@mail.ru>
2022-08-25 09:17:06 +1100

    correct HW6, add exx 43

--- Seminar/HW6/Program.cs
+++ Seminar/HW6/Program.cs
@@ -32,11 +32,36 @@
 // 3 -> 11
 // 2 -> 10
 
-Console.Write("Input Decimal Number: ");
-int decimalNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write($"The Binary code of Decimal number {decimalNumber} is : "); 
-string binNumber = "";
+// Console.Write("Input Decimal Number: ");
+// int decimalNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"The Binary code of Decimal number {decimalNumber} is : "); 
+// string binNumber = "";
 
-for (int temp = decimalNumber; temp !=0; decimalNumber = decimalNumber/2, temp = decimalNumber) binNumber += Convert.ToString(decimalNumber % 2);
+// for (int temp = decimalNumber; temp !=0; decimalNumber = decimalNumber/2, temp = decimalNumber) binNumber += Convert.ToString(decimalNumber % 2);
 
-Console.Write(binNumber); 
+// Console.Write(binNumber); 
+
+
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Line1 by function (y = k1 * x + b1), input b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Line1 by function (y = k1 * x + b1), input k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Line2 by function (y = k2 * x + b2), input b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Line1 by function (y = k2 * x + b2), input k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine();
+
+double x = (b1 - b2) / (k2 - k1);
+double y = k1 * x + b1;
+
+
+
+Console.WriteLine($"The lines (y = {k1} * x + {b1}) and (y = {k2} * x + {b2}) have intersection in point with coordinates ({x}; {y})");
